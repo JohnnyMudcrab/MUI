61a2944d85ce25b0061a2a1c2bc898400bb90763
Magnus Hanses <hanses.magnus@gmail.com>
2013-08-22 18:59:05 +0200

    made MUI work from inside packages

--- @framework/framework.m
+++ @framework/framework.m
@@ -2,18 +2,26 @@ classdef framework < handle
    
   properties
     hMainGui; 
+    packageName;
     hButton;
   end
   
   
   methods(Access = public)
 
-    function this = framework(config)
+    function this = framework(config, packageName)
       
       % check input parameter
       if nargin < 1
         config = 'example.cfg';
       end
+      
+      if nargin < 2
+        packageName = '';
+      end
+      
+      % set package name
+      this.packageName = packageName;
       
       % parse config file      
       this.parseConfig(config);
@@ -51,32 +59,13 @@ classdef framework < handle
         parent = this.hMainGui;
       end
       
-      if ~(exist('+callbacks','dir') == 7)
-        mkdir('+callbacks')
-      end
-      
-      
-      if ~(exist(['+callbacks/button_', name, '.m'],'file') == 2)
-        fcallback = fopen(['+callbacks/button_', name, '.m'],'w');
-        fprintf(fcallback, '%s\n', ['function button_', name, '(this)']);
-        fprintf(fcallback, '%s\n', ['  msgbox(''button_', name, ''',''Callback Test'',''help'');']);
-        fprintf(fcallback, '%s\n\n', 'end');
-        fclose(fcallback); 
-        
-        while(~exist(['+callbacks/button_', name, '.m'],'file'))
-          pause(0.1)
-        end
-        
-      end
-      
-      % create callback function
-      func = str2func(['callbacks.button_', name]);
+      hFunction = this.createCallback('button', name, this.packageName);
       
       % create button
       uicontrol('Style', 'pushbutton',...
                 'String', string,...
                 'Position', [position(1) - 0.5 * size(1), position(2) - 0.5 * size(2), size],...
-                'Callback', @(src,event)func(this), ...
+                'Callback', @(src,event)hFunction(this), ...
                 'Parent', parent);
       
     end
@@ -113,13 +102,84 @@ classdef framework < handle
 
     end
     
+    
+    
     function showGui(this)
       set(this.hMainGui,'Visible','on');
     end
     
     guiInit(this)
     
-  end
+    end
+  
+    methods(Static)
+      
+      function hFunction = createCallback(type, name, packageName)
+        
+        % check if framework is inside a package
+        if isempty(packageName)
+        
+          % check if callback dir exists
+          if ~(exist('+callbacks','dir') == 7)
+            
+            % if not create it
+            mkdir('+callbacks')
+            
+          end
+          
+          % create dummy callback function if non exists yet
+          if ~(exist(['+callbacks/button_', name, '.m'],'file') == 2)
+            fcallback = fopen(['+callbacks/button_', name, '.m'],'w');
+            fprintf(fcallback, '%s\n', ['function button_', name, '(this)']);
+            fprintf(fcallback, '%s\n', ['  msgbox(''button_', name, ''',''Callback Test'',''help'');']);
+            fprintf(fcallback, '%s\n\n', 'end');
+            fclose(fcallback); 
+
+            % wait until file is written
+            while(~exist(['+callbacks/button_', name, '.m'],'file'))
+              pause(0.1)
+            end
+
+          end
+
+          % create callback function
+          hFunction = str2func(['callbacks.button_', name]);
+        
+        else
+          
+          % check if callback dir exists
+          if ~(exist(['+' packageName '/+callbacks'],'dir') == 7)
+            
+            % if not create it
+            mkdir(['+' packageName '/+callbacks'])
+            
+          end
+          
+          % create dummy callback function if non exists yet
+          if ~(exist(['+' packageName '/+callbacks/button_', name, '.m'],'file') == 2)
+            fcallback = fopen(['+' packageName '/+callbacks/button_', name, '.m'],'w');
+            fprintf(fcallback, '%s\n', ['function button_', name, '(this)']);
+            fprintf(fcallback, '%s\n', ['  msgbox(''button_', name, ''',''Callback Test'',''help'');']);
+            fprintf(fcallback, '%s\n\n', 'end');
+            fclose(fcallback); 
+
+            % wait until file is written
+            while(~exist(['+' packageName '/+callbacks/button_', name, '.m'],'file'))
+              pause(0.1)
+            end
+
+          end
+
+          % create callback function
+          hFunction = str2func([packageName '.callbacks.button_', name]);
+          
+          
+          
+        end
+        
+      end
+      
+    end
   
   
 end
